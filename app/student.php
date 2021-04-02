a912bd7df56c1c9b22fa5e1f505cb04ba11a9833
hazim esa <noorhazimesa@gmail.com>
2021-04-02 10:25:22 +0800

    update package and use krlove generator for model

--- app/student.php
+++ app/student.php
@@ -4,7 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class student extends Model
+/**
+ * @property integer $id
+ * @property string $name
+ * @property string $description
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Student extends Model
 {
-    //
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'description', 'created_at', 'updated_at'];
+
 }
